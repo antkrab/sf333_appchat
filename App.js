9197457d2fb6a7579f21c177ff4a3d50ca05a8f4
Hanazono <intuch68@gmail.com>
2024-01-05 03:09:35 +0700

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
+import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import RootNavigator from "./src/navigation/RootNavigation.js";
+import { useState } from 'react';
+import UserContext from './src/context/UserContext.js';
+
 export default function App() {
+  const [username, setUsername] = useState("");
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <StatusBar />
+      <UserContext.Provider value={ { username, setUsername } }>
+      <RootNavigator/>
+      </UserContext.Provider>
+    </NavigationContainer>
   );
 }
 
