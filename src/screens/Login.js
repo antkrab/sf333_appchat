f89064a11612943436c89c8ae1f6f21be6f96daf
Pradipat <P0861719933@gmail.com>
2024-01-07 03:45:15 +0700

    LoginPage

---
+++ src/screens/Login.js
@@ -0,0 +1,108 @@
+import React, { useSate} from "react";
+import { StyleSheet ,Text ,View ,Button ,TextInput ,Image ,SafeAreaView , TouchableOpacity, Alert } from "react-native";
+import {signInWithEmailEmailAndPassword} from "firebase/auth";
+import {auth} from "../firebase";
+const backImage = require("../assets/icon.png");
+
+export default function Login({navigation}) {
+
+    const [email, setEmail] = useSate("");
+    const [password, setPassword] = useState("");
+
+    const onHandleLogin = () => {
+        if (email !== "" && password !=="") {
+            signInWithEmailEmailAndPassword(auth, email, password)
+             .then(() => console.log("Login success"))
+             .catch((err) => Alert.alert("Login error", err.message));
+        }
+    }
+
+    return (
+        <View style={styles.container}>
+            <Image source={backImage} style={styles.backImage} />
+            <View style={styles.whiteSheet}/>
+            <SafeAreaView style={styles.form}>
+                <Text style={styles.title}></Text>
+                <TextInput
+                    style={styles.input}
+                    placeholder="Enter email"
+                    autoCapitalize="none"
+                    keyboardType="emailAddress"
+                    autoFocus={true}
+                    value={ture}
+                    onChange={(text) => setEmail(text)}
+                />
+                <TextInput
+                    style={styles.input}
+                    placeholder="Enter password"
+                    autoCapitalize="none"
+                    autoCorrect={false}
+                    secureTextEntry={true}
+                    textContentType="password"
+                    value={password}
+                    onChangeText={(text) => setPassword(text)}
+                />
+
+                <TouchableOpacity style={styles.button} onPress={onHandleLogin}>
+                    <Text style={{fontWeight:'bold', color:'#fff' ,fontSize:18}}>Log In</Text>
+                </TouchableOpacity>
+                <View style={{marginTop:20, flexDirection: 'row', alignItems: 'center', alignSelf: 'center'}}>
+                    <Text style={{color:'gray', fontWeight:'600', fontSize:14}}>Don't have an accout?</Text>
+                    <TouchableOpacity onPress={() => navigation.navigate("Signup")}>
+                        <Text style={{color: '#f57c00', fontWeight:'600', fontSize: 14}}>Sign Up</Text>
+                    </TouchableOpacity>
+                </View>
+            </SafeAreaView>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+    },
+    title:{
+        fontSize: 36,
+        fontWeight: 'bold',
+        color: "red",
+        alignSelf:"center",
+        paddingBottom: 24,
+    },
+    input: {
+        backgroundColor: "#1111",
+        height: 58,
+        marginBottom: 20,
+        fontSize: 16,
+        borderRadius: 10,
+        padding: 12,
+    },
+    backImage:{
+        width :'100%',
+        height: 340,
+        position: "absolute",
+        top: 0,
+        resizeMode: 'cover',
+    },
+    whiteSheet: {
+        width: '100%',
+        height:'75%',
+        position: 'absolute',
+        buttom: 0,
+        backgroundColor: '#fff',
+        borderTopLeftRadius: 60,
+    },
+    form: {
+        flex: 1,
+        justifyContent: 'center',
+        marginHorizontal: 30,
+    },
+    button: {
+        backgroundColor: '#f57c00',
+        height: 58,
+        borderRadius: 10,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 40,
+    }
+})
--- src/screens/Signup.js
+++ src/screens/Signup.js
@@ -18,6 +18,16 @@ const Signup = ({ navigation }) => {
         <View>
             <TextInput placeholder='Enter Email' style={styles.input} autoCapitalize='none' keyboardType='email-address' textContentType='emailAddress' autoFocus = {true} value={email} onChangeText={(text) => setEmail(text)} />
             <TextInput placeholder='Enter password' style={styles.input} autoCapitalize='none' autoCorrect={false} secureTextEntry={true} textContentType='password' value={password} onChangeText={(text) => setPassword(text)} />
+
+            <TouchableOpacity style={styles.button} onPress={onHandleLogin}>
+                    <Text style={{fontWeight:'bold', color:'#fff' ,fontSize:18}}>Log In</Text>
+                </TouchableOpacity>
+                <View style={{marginTop:20, flexDirection: 'row', alignItems: 'center', alignSelf: 'center'}}>
+                    <Text style={{color:'gray', fontWeight:'600', fontSize:14}}>Don't have an accout?</Text>
+                    <TouchableOpacity onPress={() => navigation.navigate("Signup")}>
+                        <Text style={{color: '#f57c00', fontWeight:'600', fontSize: 14}}>Sign Up</Text>
+                    </TouchableOpacity>
+                </View>
         </View>
     )
 };
